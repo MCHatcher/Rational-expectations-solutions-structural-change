14543742b1dd0fc8a571d07653060bfe9d1b2b6f
Michael Hatcher <m.c.hatcher@soton.ac.uk>
2022-02-18 11:12:41 +0000

    Recursive solution algorithm - Type 2 credibility

--- General_case_date_0_announcement_type_cred.m
+++ General_case_date_0_announcement_type_cred.m
@@ -1,7 +1,8 @@
-%Announcement in the first period 
+%Announcement in the first period, Type 2 Credibility, forward guidance 
 %Ireland (2007) NK Model, replicates Cagliarini and Kulish (2013, Fig 3)
 %To study a different example, simply change the parameters and matrices
 %Model structures are defined in the 'Insert' files
+%Written by Michael Hatcher (m.c.hatcher@soton.ac.uk). Any errors are my own.
 
 clc; clear; %close all;
 
@@ -32,17 +33,17 @@ Omeg_IC = Omega_tild; Gama_IC = Gama_tild; Psi_IC = Psi_tild;
 %Computation of matrix recursion
  for t=T_sim:-1:1     
      
-    B1t = ind(t,1)*B1 + (1-ind(t,1))*B1_tild;
-    B2t = ind(t,1)*B2 + (1-ind(t,1))*B2_tild;
-    B3t = ind(t,1)*B3 + (1-ind(t,1))*B3_tild;
-    B4t = ind(t,1)*B4 + (1-ind(t,1))*B4_tild;
-    B5t = ind(t,1)*B5 + (1-ind(t,1))*B5_tild;
+    B1t = ind(t)*B1 + (1-ind(t))*B1_tild;
+    B2t = ind(t)*B2 + (1-ind(t))*B2_tild;
+    B3t = ind(t)*B3 + (1-ind(t))*B3_tild;
+    B4t = ind(t)*B4 + (1-ind(t))*B4_tild;
+    B5t = ind(t)*B5 + (1-ind(t))*B5_tild;
             
-    B1t_IC = ind_IC(t,1)*B1 + (1-ind_IC(t,1))*B1_tild;
-    B2t_IC = ind_IC(t,1)*B2 + (1-ind_IC(t,1))*B2_tild;
-    B3t_IC = ind_IC(t,1)*B3 + (1-ind_IC(t,1))*B3_tild;
-    B4t_IC = ind_IC(t,1)*B4 + (1-ind_IC(t,1))*B4_tild;
-    B5t_IC = ind_IC(t,1)*B5 + (1-ind_IC(t,1))*B5_tild;
+    B1t_IC = ind_IC(t)*B1 + (1-ind_IC(t))*B1_tild;
+    B2t_IC = ind_IC(t)*B2 + (1-ind_IC(t))*B2_tild;
+    B3t_IC = ind_IC(t)*B3 + (1-ind_IC(t))*B3_tild;
+    B4t_IC = ind_IC(t)*B4 + (1-ind_IC(t))*B4_tild;
+    B5t_IC = ind_IC(t)*B5 + (1-ind_IC(t))*B5_tild;
          
    Omeg_star = (B1t - B2t*Omeg_star) \ B3t; 
    Gama_star = (B1t - B2t*Omeg_star) \ B4t; 
@@ -83,15 +84,15 @@ Lambda = 0.7;
 %Computation of matrix recursion
  for t=T_sim:-1:1       
             
-    B1t = ind(t,1)*B1 + (1-ind(t,1))*B1_tild;
-    B2t = ind(t,1)*B2 + (1-ind(t,1))*B2_tild;
-    B3t = ind(t,1)*B3 + (1-ind(t,1))*B3_tild;
-    B4t = ind(t,1)*B4 + (1-ind(t,1))*B4_tild;
-    B5t = ind(t,1)*B5 + (1-ind(t,1))*B5_tild;
+    B1t = ind(t)*B1 + (1-ind(t))*B1_tild;
+    B2t = ind(t)*B2 + (1-ind(t))*B2_tild;
+    B3t = ind(t)*B3 + (1-ind(t))*B3_tild;
+    B4t = ind(t)*B4 + (1-ind(t))*B4_tild;
+    B5t = ind(t)*B5 + (1-ind(t))*B5_tild;
     
     if t <= T_tild
         B1t = B1t - B2t*(1-Lambda)*Omeg_tild_IC_t(:,:,t+1);
-        B2t = Lambda*B2t;
+        B2t = B2t*Lambda;
         B5t = B5t + B2t*(1-Lambda)*Psi_tild_IC_t(:,:,t+1);
     end
          
