8a5b3a30368566e77a4d1c15ace890a3afa7103c
Michael Hatcher <m.c.hatcher@soton.ac.uk>
2022-02-18 11:58:10 +0000

    Plotter

--- Pension_reform_plotter.m
+++ Pension_reform_plotter.m
@@ -13,6 +13,6 @@ end
 
 set(0,'DefaultLineLineWidth',1.5)
 figure(1)
-subplot(1,3,1), plot(Periods(1:12),cy(1:12),'k'), hold on, plot(Periods(1:12),cyu(1:12),'--k'), title('Consumption (young)')
-subplot(1,3,2), plot(Periods(1:12),co(1:12),'k'),hold on, plot(Periods(1:12),cou(1:12),'--k'), title('Consumption (old)')
-subplot(1,3,3), hold on, plot(Periods(1:12), k(1:12),'k'), hold on, plot(Periods(1:12),ku(1:12),'--k'), title('Capital')
+subplot(1,3,1), plot(Periods(1:12),cy(1:12),'k'), hold on, plot(Periods(1:12),cyu(1:12),'--k'), title('Consumption (young): $\hat{c}_{t,y}$','interpreter','latex'), xlabel('Time,t')
+subplot(1,3,2), plot(Periods(1:12),co(1:12),'k'),hold on, plot(Periods(1:12),cou(1:12),'--k'), title('Consumption (old): $\hat{c}_{t,o}$', 'interpreter','latex'), xlabel('Time,t')
+subplot(1,3,3), hold on, plot(Periods(1:12), k(1:12),'k'), hold on, plot(Periods(1:12),ku(1:12),'--k'), title('Capital: $\hat{k}_{t+1}$','interpreter','latex'), xlabel('Time,t')
