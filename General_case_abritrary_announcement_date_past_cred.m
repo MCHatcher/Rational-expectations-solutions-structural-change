4f0e7482e52879f232551dbc19812e869b257603
Michael Hatcher <m.c.hatcher@soton.ac.uk>
2022-02-18 10:57:21 +0000

    Recursive solution algorithm - base credibility

--- General_case_abritrary_announcement_date_past_cred.m
+++ General_case_abritrary_announcement_date_past_cred.m
@@ -1,7 +1,8 @@
-%Arbitrary announcement date T_ann, applied to:
+%Arbitrary announcement date T_ann, imperfect credibility applied to:
 %Ireland (2007) NK Model, replicates Cagliarini and Kulish (2013, Fig 3)
 %To study a different example, simply change the parameters and matrices
 %Model structures are defined in the 'Insert' files
+%Written by Michael Hatcher (m.c.hatcher@soton.ac.uk). Any errors are my own.
 
 clc; clear; %close all;
 
@@ -25,30 +26,30 @@ Omeg = Omega_tild; Gama = Gama_tild; Psi = Psi_tild;
 %Computation of matrix recursion
  for t=T_sim:-1:1       
             
-    B1t = ind(t,1)*B1 + (1-ind(t,1))*B1_tild;
-    B2t = ind(t,1)*B2 + (1-ind(t,1))*B2_tild;
-    B3t = ind(t,1)*B3 + (1-ind(t,1))*B3_tild;
-    B4t = ind(t,1)*B4 + (1-ind(t,1))*B4_tild;
-    B5t = ind(t,1)*B5 + (1-ind(t,1))*B5_tild;
+    B1t = ind(t)*B1 + (1-ind(t))*B1_tild;
+    B2t = ind(t)*B2 + (1-ind(t))*B2_tild;
+    B3t = ind(t)*B3 + (1-ind(t))*B3_tild;
+    B4t = ind(t)*B4 + (1-ind(t))*B4_tild;
+    B5t = ind(t)*B5 + (1-ind(t))*B5_tild;
     
     %for solution refinements
     
-    Lambda = [0.7 0 0 0 0 0; 0 1 0 0 0 0; 0 0 1 0 0 0; 0 0 0 1 0 0; 0 0 0 0 1 0; 0 0 0 0 0 1];
+    Lambda = [0.70 0 0 0 0 0; 0 1 0 0 0 0; 0 0 1 0 0 0; 0 0 0 1 0 0; 0 0 0 0 1 0; 0 0 0 0 0 1];
     %Lambda = eye(6);  %for perfect credibility case
     
     if t >= T_tild+1
         Lambda = eye(6);
     end
     
-    B1t = B1t - (eye(length(B1t))-Lambda)*F0;
-    B2t = Lambda*B2t;
-    B3t = B3t + (eye(length(B1t))-Lambda)*F1;
-    B4t = B4t + (eye(length(B1t))-Lambda)*F2;
-    B5t = B5t + (eye(length(B1t))-Lambda)*F3;
+    B1t_hat = B1t - B2t*(eye(length(B1t))-Lambda)*F0;
+    B2t_hat = B2t*Lambda;
+    B3t_hat = B3t + B2t*(eye(length(B1t))-Lambda)*F1;
+    B4t_hat = B4t + B2t*(eye(length(B1t))-Lambda)*F2;
+    B5t_hat = B5t + B2t*(eye(length(B1t))-Lambda)*F3;
          
-   Omeg = (B1t - B2t*Omeg) \ B3t; 
-   Gama = (B1t - B2t*Omeg) \ B4t; 
-   Psi = (B1t - B2t*Omeg) \ (B2t*Psi + B5t);    
+   Omeg = (B1t_hat - B2t_hat*Omeg) \ B3t_hat; 
+   Gama = (B1t_hat - B2t_hat*Omeg) \ B4t_hat; 
+   Psi = (B1t_hat - B2t_hat*Omeg) \ (B2t_hat*Psi + B5t_hat);    
         
     if t >= T_tild+1
         Omeg = Omega_tild;
