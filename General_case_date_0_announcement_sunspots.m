a05b29aa9fe44c6922ff35b8b595dcadc84736dc
Michael Hatcher <m.c.hatcher@soton.ac.uk>
2022-02-18 11:07:56 +0000

    Recursive solution algorithm - indeterminacy

--- General_case_date_0_announcement_sunspots.m
+++ General_case_date_0_announcement_sunspots.m
@@ -2,6 +2,7 @@
 %Ireland (2007) NK Model, replicates Cagliarini and Kulish (2013, Fig 3)
 %To study a different example, simply change the parameters and matrices
 %Model structures are defined in the 'Insert' files
+%Written by Michael Hatcher (m.c.hatcher@soton.ac.uk). Any errors are my own.
 
 clc; clear; %close all;
 
@@ -25,11 +26,11 @@ Omeg = Omega_tild; Gama = Gama_tild; Psi = Psi_tild;
 %Computation of matrix recursion
  for t=T_sim:-1:1       
             
-    B1t = ind(t,1)*B1 + (1-ind(t,1))*B1_tild;
-    B2t = ind(t,1)*B2 + (1-ind(t,1))*B2_tild;
-    B3t = ind(t,1)*B3 + (1-ind(t,1))*B3_tild;
-    B4t = ind(t,1)*B4 + (1-ind(t,1))*B4_tild;
-    B5t = ind(t,1)*B5 + (1-ind(t,1))*B5_tild;
+    B1t = ind(t)*B1 + (1-ind(t))*B1_tild;
+    B2t = ind(t)*B2 + (1-ind(t))*B2_tild;
+    B3t = ind(t)*B3 + (1-ind(t))*B3_tild;
+    B4t = ind(t)*B4 + (1-ind(t))*B4_tild;
+    B5t = ind(t)*B5 + (1-ind(t))*B5_tild;
          
    Omeg = (B1t - B2t*Omeg) \ B3t; 
    Gama = (B1t - B2t*Omeg) \ B4t; 
